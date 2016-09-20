6ecc484e969a6554beab82ee9640897652881ab4
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2016-09-20 09:02:55 -0700

    BrainStation first commit

---
+++ BrainStation/BrainStation.h
@@ -0,0 +1,34 @@
+/*
+ * This is the source code of Brain Programming Language.
+ * It is licensed under GNU GPL v. 3 or later.
+ * You should have received a copy of the license in this archive (see LICENSE).
+ *
+ * Copyright Brain, 2016.
+ */
+
+#ifndef BRAINSTATION_H
+#define BRAINSTATION_H
+
+// include types & constants of Wiring core API
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#include <BrainDelegate.h>
+
+/**
+ * @brief Represents the Brain Station Console and also provide functions to be
+ * used in the Arduino environment.
+ */
+class BrainStation : public BrainDelegate
+{
+public:
+    ~BrainStation() {}
+    void update(Brain *brain);
+private:
+};
+
+#endif // BRAINSTATION_H
+
